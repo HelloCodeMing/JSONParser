e847257699bce61c249d7bda830aeaf176c0088d
HelloCodeMing <huanmingwong@163.com>
2014-10-14 09:34:57 +0800

    my own version

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 
 fs.readFile("./test.json", "utf-8", function(err, data) {
 	if (!err) {
-		var res = Json.json_parse(data);
+		var res = Json.parse(data);
 		console.log(res);
 	} else {
 		console.assert(err);
